52bcce72b1b7ed989e371f2910e1566efd28f619
Felipe Lacerda <joaofelipeamorim2001@gmail.com>
2024-06-23 17:30:40 -0300

    new_lines

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Felipe Lacerda',
     author_email='joaofelipe.amorim@aluno.uece.br',
-    url='<https://github.com/FelipeLacerdaAmorim/PyLibTest',
+    url='https://github.com/FelipeLacerdaAmorim/PyLibTest',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
